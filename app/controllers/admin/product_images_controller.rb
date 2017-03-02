88a9e26e41c742c55b67ded36911340b7c6aa2c3
wuhongye <1253697896@qq.com>
2017-03-02 17:46:43 +0800

    update

--- app/controllers/admin/product_images_controller.rb
+++ app/controllers/admin/product_images_controller.rb
@@ -1,16 +1,29 @@
 class Admin::ProductImagesController < Admin::BaseController
   
+  before_action :find_product
+
   def index
-    @product = Product.find(params[:id])
     @product_images = @product.product_images
   end
 
   def create
-    
+    params[:images].each do |image|
+      @product.product_images << ProductImage.new(image: image)
+    end
+
+    redirect_to :back
   end
 
   def destroy
-    
+    @product_image = @product.product_images.find(params[:id])
+    if @product_image.destroy
+      flash[:notice] = "删除成功"
+      redirect_to :back
+    end
   end
 
+  private
+  def find_product
+    @product = Product.find params[:product_id]
+  end
 end
