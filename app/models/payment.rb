058e03bb352c661eafae0e28778e9eb6335daeb4
wuhongye <1253697896@qq.com>
2017-03-09 17:58:47 +0800

    update

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -33,6 +33,39 @@ class Payment < ApplicationRecord
     payment
   end
 
+  def is_success?
+    self.status == PaymentStatus::Success
+  end
+
+  def do_success_payment! options
+    self.transaction do
+      self.transaction_no = options[:trade_no]
+      self.status = Payment::PaymentStatus::Success
+      self.raw_response = options.to_json
+      self.payment_at = Time.now
+      self.save!
+
+      # 更新订单状态
+      self.orders.each do |order|
+        if order.is_paid?
+          raise "order #{order.order_no} has already been paid"
+        end
+
+        order.status = Order::OrderStatus::Paid
+        order.payment_at = Time.now
+        order.save!
+      end
+    end
+  end
+
+  def do_failed_payment! options
+    self.transaction_no = options[:trade_no]
+    self.status = Payment::PaymentStatus::Failed
+    self.raw_response = options.to_json
+    self.payment_at = Time.now
+    self.save!
+  end
+
   private
   def gen_payment_no
     self.payment_no = RandomCode.generate_utoken(32)
